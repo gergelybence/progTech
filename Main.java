3edd6a0f449cba8387d39b1f2ea692f455515c4b
gergelybence <113361443+gergelybence@users.noreply.github.com>
2023-09-08 15:53:24 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+	
+	public static void main(String[] args) {
+		GreetingsGenerator generator = new GreetingsGenerator();
+		System.out.println(generator.generateGreetings("Uram"));
+	}
+	
+}
